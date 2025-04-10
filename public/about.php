92fc616f4ad7f639c8c6459bf3a105667107e833
fmixolydian <fmixolydian@proton.me>
2025-04-10 21:03:16 +0200

    FUCK i forgot to add the file

---
+++ public/about.php
@@ -0,0 +1,26 @@
+<?php
+
+require '../header.php';
+
+?>
+
+<style>
+	article {
+		background-color: #222;
+		width: calc(800px - 10px * 2);
+		padding: 10px;
+		min-height: 200px;
+	}
+</style>
+
+<article>
+	<h1>About</h1>
+	<p>Beepboard is made by <code>fmixolydian</code>, with the help of a few contributors:</p>
+	<ul>
+		<li><code>spongycool</code>: icons and other art</li>
+	</ul>
+</article>
+			
+		</main>
+	</body>
+</html>
