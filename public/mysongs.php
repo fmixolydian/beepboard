9622eaf5d960ad2f18905e1d6f5520ac133b3274
fmixolydian <fmixolydian@proton.me>
2025-04-13 22:49:10 +0200

    bugfix4

--- public/mysongs.php
+++ public/mysongs.php
@@ -3,10 +3,6 @@
 require '../config.php';
 require '../functions.php';
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 if (array_key_exists("token", $_COOKIE)) {
 	
 	$data = BB_getUserdataByToken($_COOKIE['token']);
